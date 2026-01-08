f6d5293904e5002c3c79000e08a2563cb1c8a981
Gaurav132005 <gauravkkr345@gmail.com>
2026-01-09 00:24:22 +0530

    Completed the almost

---
+++ Backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://LereLundke:TMKC3BAAR@cluster0.5f5cxbz.mongodb.net/todos')
+
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todo = mongoose.model('todos', todoSchema);
+
+module.exports = {
+    todo
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,76 @@
 const express = require('express');
+const { todo } = require('./db');
+const { createTodo, updateTodo } = require('./types');
+const cors = require('cors');
+
 const app = express();
-const zod = require('./types');
+
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
+
+    // Input validation is happening here
+    const createPayload = req.body;
+    const validatedPayload = createTodo.safeParse(createPayload);
+
+    if(!validatedPayload.success) {
+        res.status(411).json({
+            msg: "You are not sending correct inputs",
+        })
+        return;
+    }
+
+
+    // Now we will put the data into mongo db.
+
+    await todo.create({
+        title: validatedPayload.data.title,
+        description: validatedPayload.data.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "Todo has been created Successfully"
+    })
+
+})
+
+
+app.put("/todos", async (req, res) => {
+
+    // input validation is happening here
+    const updatePayload = req.body;
+    const validateUpdatedPayload = updateTodo.safeParse(updatePayload);
+
+    if(!validateUpdatedPayload.success) {
+        res.status(411).json({
+            msg: "You are not sending the correct input",
+        })
+        return;
+    }
+
+    // update in mongodb here
+
+    await todo.update({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+
+    res.json({
+        msg: "todo marked as completed"
+    })
     
 })
 
+app.get("/todos", async (req, res) => {
+    const todos = await todo.find({}); //it returns promise so we have to use async
 
-app.put("/todos", (req, res) => {
-
-})
-
-app.get("/todos", (req, res) => {
+    res.json({
+        todos
+    })
 
 })
 
